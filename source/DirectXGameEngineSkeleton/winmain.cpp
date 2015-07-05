b9800ea37cd1c66ef26de9f1dece7f472c3541dd
Trần Lê Tuấn Anh <tranletuananh1992@gmail.com>
2015-07-05 19:35:16 +0700

    Add DirectX Graphics functionality
    New classes:
    - Graphics: a DirectX graphics manager class.
    - GameError.

--- source/DirectXGameEngineSkeleton/winmain.cpp
+++ source/DirectXGameEngineSkeleton/winmain.cpp
@@ -1,10 +1,14 @@
+#define  _CRTDBG_MAP_ALLOC			// For detecting memory leaks
 #define WIN32_LEAN_AND_MEAN
 
-#include <windows.h>
+#include <Windows.h>
+#include <stdlib.h>					// For detecting memory leaks
+#include <crtdbg.h>					// For detecting memory leaks
+#include "graphics.h"
 
 // Function prototypes
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int);
-bool CreateMainWindow(HINSTANCE, int);
+bool CreateMainWindow(HWND &, HINSTANCE, int);
 LRESULT WINAPI WinProc(HWND, UINT, WPARAM, LPARAM);
 bool AnotherInstance();
 
@@ -14,12 +18,7 @@ HDC hdc;							// Handle to device context
 TCHAR ch = ' ';						// Character entered
 RECT rect;							// Rectangle structure
 PAINTSTRUCT ps;						// Used in WM_PAINT
-
-// Constants
-const char CLASS_NAME[] = "WinMain";
-const char APP_TITLE[] = "Hello World";   // title bar text
-const int  WINDOW_WIDTH = 400;             // width of window
-const int  WINDOW_HEIGHT = 400;             // height of window
+Graphics *graphics;
 
 //=============================================================================
 // Starting point for a Windows application
@@ -34,29 +33,51 @@ int WINAPI WinMain(HINSTANCE hInstance,
 	LPSTR     lpCmdLine,
 	int       nCmdShow)
 {
+	// Check for memory leak if debug build
+#if defined(DEBUG) | defined(_DEBUG)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif // defined(DEBUG) | defined(_DEBUG)
+
 	MSG	 msg;
+	HWND hwnd = NULL;
 
 	// Create the window
-	if (!CreateMainWindow(hInstance, nCmdShow))
-		return false;
-
-	// main message loop
-	int done = 0;
-	while (!done)
-	{
-		// PeekMessage is a non-blocking method for checking for Windows messages.
-		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+	if (!CreateMainWindow(hwnd, hInstance, nCmdShow))
+		return 1;
+	try {
+		// Create Graphics object
+		graphics = new Graphics();
+		// Initialize Graphics, throws GameError
+		graphics->initialize(hwnd, GAME_WIDTH, GAME_HEIGHT, FULLSCREEN);
+		// main message loop
+		int done = 0;
+		while (!done)
 		{
-			//look for quit message
-			if (msg.message == WM_QUIT)
-				done = 1;
+			// PeekMessage is a non-blocking method for checking for Windows messages.
+			if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+			{
+				//look for quit message
+				if (msg.message == WM_QUIT)
+					done = 1;
 
-			//decode and pass messages on to WinProc
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
+				//decode and pass messages on to WinProc
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+			else
+				graphics->showBackbuffer();
 		}
 	}
-	return msg.wParam;
+	catch (const GameError &err)
+	{
+		MessageBox(NULL, err.getMessage(), "Error", MB_OK);
+	}
+	catch (...)
+	{
+		MessageBox(NULL, "Unknown error occured in game.", "Error", MB_OK);
+	}
+	safeDelete(graphics);  // free memory before exit
+	return 0;
 }
 
 //=============================================================================
@@ -66,45 +87,29 @@ LRESULT WINAPI WinProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	switch (msg)
 	{
-		case WM_DESTROY:
+	case WM_DESTROY:
+		//tell Windows to kill this program
+		PostQuitMessage(0);
+		return 0;
+	case WM_CHAR:               // a character was entered by the keyboard
+		switch (wParam)         // the character is in wParam
+		{
+		case ESC_KEY:       // exit program key
 			//tell Windows to kill this program
 			PostQuitMessage(0);
 			return 0;
-		case WM_CHAR:
-			switch (wParam)
-			{
-			case 0x08:	// Backspace
-			case 0x09:	// Tab
-			case 0x0A:	// Line feed
-			case 0x0D:	// Carriage return
-			case 0x1B:	// Escape
-				MessageBeep((UINT)-1);	// Beep but do not display
-				return 0;
-			default:		// Displayable character
-				ch = (TCHAR)wParam;		// Get the character
-				InvalidateRect(hWnd, NULL, TRUE);	// Force WM_PAINT
-				return 0;
-			}
-		case WM_PAINT:				// The window needs to be redrawn
-			hdc = BeginPaint(hWnd, &ps);		// Get handle to device context
-			GetClientRect(hWnd, &rect);			// Get the window rectangle
-			// Display the character
-			TextOut(hdc, rect.right / 2, rect.bottom / 2, &ch, 1);
-			EndPaint(hWnd, &ps);
-			return 0;
-		default:
-			return DefWindowProc(hWnd, msg, wParam, lParam);
+		}
 	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
 }
 
 //=============================================================================
 // Create the window
 // Returns: false on error
 //=============================================================================
-bool CreateMainWindow(HINSTANCE hInstance, int nCmdShow)
+bool CreateMainWindow(HWND &hwnd, HINSTANCE hInstance, int nCmdShow)
 {
 	WNDCLASSEX wcx;
-	HWND hwnd;
 
 	// Fill in the window class structure with parameters 
 	// that describe the main window. 
@@ -116,7 +121,7 @@ bool CreateMainWindow(HINSTANCE hInstance, int nCmdShow)
 	wcx.hInstance = hInstance;          // handle to instance 
 	wcx.hIcon = NULL;
 	wcx.hCursor = LoadCursor(NULL, IDC_ARROW);   // predefined arrow 
-	wcx.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);    // black background brush 
+	wcx.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);    // black background 
 	wcx.lpszMenuName = NULL;           // name of menu resource 
 	wcx.lpszClassName = CLASS_NAME;     // name of window class 
 	wcx.hIconSm = NULL;                 // small class icon 
@@ -126,15 +131,22 @@ bool CreateMainWindow(HINSTANCE hInstance, int nCmdShow)
 	if (RegisterClassEx(&wcx) == 0)    // if error
 		return false;
 
+	//set up the screen in windowed or fullscreen mode?
+	DWORD style;
+	if (FULLSCREEN)
+		style = WS_EX_TOPMOST | WS_VISIBLE | WS_POPUP;
+	else
+		style = WS_OVERLAPPEDWINDOW;
+
 	// Create window
 	hwnd = CreateWindow(
 		CLASS_NAME,             // name of the window class
-		APP_TITLE,              // title bar text
-		WS_OVERLAPPEDWINDOW,    // window style
+		GAME_TITLE,             // title bar text
+		style,                  // window style
 		CW_USEDEFAULT,          // default horizontal position of window
 		CW_USEDEFAULT,          // default vertical position of window
-		WINDOW_WIDTH,           // width of window
-		WINDOW_HEIGHT,          // height of the window
+		GAME_WIDTH,             // width of window
+		GAME_HEIGHT,            // height of the window
 		(HWND)NULL,            // no parent window
 		(HMENU)NULL,           // no menu
 		hInstance,              // handle to application instance
@@ -143,6 +155,19 @@ bool CreateMainWindow(HINSTANCE hInstance, int nCmdShow)
 	// if there was an error creating the window
 	if (!hwnd)
 		return false;
+
+	if (!FULLSCREEN)             // if window
+	{
+		// Adjust window size so client area is GAME_WIDTH x GAME_HEIGHT
+		RECT clientRect;
+		GetClientRect(hwnd, &clientRect);   // get size of client area of window
+		MoveWindow(hwnd,
+			0,                                           // Left
+			0,                                           // Top
+			GAME_WIDTH + (GAME_WIDTH - clientRect.right),    // Right
+			GAME_HEIGHT + (GAME_HEIGHT - clientRect.bottom), // Bottom
+			TRUE);                                       // Repaint the window
+	}
 
 	// Show the window
 	ShowWindow(hwnd, nCmdShow);
